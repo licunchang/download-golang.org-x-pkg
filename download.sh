8f0552a73ef74a08b91b7cd35df59270be2336fd
licunchang <mail@licunchang.com>
2019-05-10 20:21:16 +0800

    first download package then update package

--- download.sh
+++ download.sh
@@ -13,19 +13,29 @@ log::info() {
 PACKAGES=("arch" "benchmarks" "blog" "build" "crypto" "debug" "exp" "image" "lint" "mobile" "net" "oauth2" "perf" "playground" "review" "sync" "sys" "talks" "text" "time" "tools" "tour" "vgo" "website" "xerrors")
 readonly PACKAGES
 
+PACKAGES_NEED_UPDATE=()
+
 for package in ${PACKAGES[@]}; do
-    log::info "Github project url: https://github.com/golang/$package"
     if [[ -d "$GOPATH/src/golang.org/x/$package/.git" ]]; then
-        log::info "$GOPATH/src/golang.org/x/$package is exist, update package"
-        git pull --quiet > /dev/null 2>&1
-        if [[ "$?" -ne 0 ]]; then
-            log::error "https://github.com/golang/$package update error"
-        fi
+        PACKAGES_NEED_UPDATE+=($package)
     else
+        log::info "Github project url: https://github.com/golang/$package"
+
         rm -rf "$GOPATH/src/golang.org/x/$package/"
+        log::info "    git clone git@github.com:golang/$package.git $GOPATH/src/golang.org/x/$package"
         git clone --quiet git@github.com:golang/$package.git $GOPATH/src/golang.org/x/$package > /dev/null 2>&1
         if [[ "$?" -ne 0 ]]; then
-            log::error "https://github.com/golang/$package clone error"
+            log::error "    https://github.com/golang/$package clone error"
         fi
+    fi
+done
+
+for package in ${PACKAGES_NEED_UPDATE[@]}; do
+    log::info "Github project url: https://github.com/golang/$package"
+
+    log::info "    git -C $GOPATH/src/golang.org/x/$package pull"
+    git -C $GOPATH/src/golang.org/x/$package pull --quiet > /dev/null 2>&1
+    if [[ "$?" -ne 0 ]]; then
+        log::error "    https://github.com/golang/$package pull error"
     fi
 done
