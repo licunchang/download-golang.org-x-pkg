33e6aa15f58b4740d431c6610697af9ee340d27d
licunchang <mail@licunchang.com>
2019-05-10 19:59:48 +0800

    add info

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-logger::error() {
+log::error() {
     # `date --iso-8601=ns`
     printf "%s\t%s\t%s\n" "$(date +'%Y-%m-%dT%H:%M:%S%z')" "[ERROR]" "$@" >&2
 }
 
-logger::info() {
+log::info() {
     # `date --iso-8601=ns`
     printf "%s\t%s\t%s\n" "$(date +'%Y-%m-%dT%H:%M:%S%z')" "[INFO]" "$@" >&2
 }
@@ -14,11 +14,18 @@ PACKAGES=("arch" "benchmarks" "blog" "build" "crypto" "debug" "exp" "image" "lin
 readonly PACKAGES
 
 for package in ${PACKAGES[@]}; do
+    log::info "Github project url: https://github.com/golang/$package"
     if [[ -d "$GOPATH/src/golang.org/x/$package/.git" ]]; then
-        logger::info "$GOPATH/src/golang.org/x/$package is exist."
-        git pull --quiet
+        log::info "$GOPATH/src/golang.org/x/$package is exist, update package"
+        git pull --quiet > /dev/null 2>&1
+        if [[ "$?" -ne 0 ]]; then
+            log::error "https://github.com/golang/$package update error"
+        fi
     else
         rm -rf "$GOPATH/src/golang.org/x/$package/"
-        git clone git@github.com:golang/$package.git $GOPATH/src/golang.org/x/$package
+        git clone --quiet git@github.com:golang/$package.git $GOPATH/src/golang.org/x/$package > /dev/null 2>&1
+        if [[ "$?" -ne 0 ]]; then
+            log::error "https://github.com/golang/$package clone error"
+        fi
     fi
 done
